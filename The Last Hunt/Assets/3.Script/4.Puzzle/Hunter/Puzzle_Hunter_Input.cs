c026a8e40569d8e0578ce51cf6e6e0ce55912474
정해창 <femgoon0425@gmail.com>
2024-07-09 13:34:23 +0900

    폴더 시스템 정리
    각 에셋별로 shoot, rhythm, run, puzzle 폴더 세팅

---
+++ The Last Hunt/Assets/3.Script/4.Puzzle/Hunter/Puzzle_Hunter_Input.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle_Hunter_Input : MonoBehaviour
+{
+    /// <summary>
+    /// x = 검출 앞 거리
+    /// y = 검출 높이
+    /// </summary>
+    [SerializeField]
+    Vector2 offset = new Vector2();
+
+    private void OnDrawGizmos()
+    {
+        Vector3 drawPosition = transform.position + transform.forward * offset.x + Vector3.up * offset.y;
+
+        Gizmos.DrawWireSphere(drawPosition, 1f);
+    }
+
+    /// <summary>
+    /// 타일을 들고 있는가?
+    /// </summary>
+    bool isCarrying = false;
+
+    private void Update()
+    {
+        //플레이어 앞의 특정 위치에서 Raycast를 실행
+        Vector3 drawPosition = transform.position + transform.forward * offset.x + Vector3.up * offset.y;
+
+        //아무것도 들고 있지 않을 때 검사를 시작
+        if (isCarrying)
+        {
+            
+        }  
+    }
+}
