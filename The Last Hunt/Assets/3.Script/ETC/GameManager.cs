36b00b2ddfc64fc1e8978cf780ec384435fbe9fb
정해창 <femgoon0425@gmail.com>
2024-07-23 12:31:25 +0900

    GameManager 작성
    Json으로 UserData(점수) 저장

--- The Last Hunt/Assets/3.Script/ETC/GameManager.cs
+++ The Last Hunt/Assets/3.Script/ETC/GameManager.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance;
+
+    private void Awake()
     {
-        
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+            isStoryMode = true;
+            //path = Path.Combine(Application.persistentDataPath, jsonName);
+            path = Path.Combine(Application.dataPath, "UserData.json");
+            print(path);
+            RoadJson();
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    /// <summary>
+    /// 타이틀 화면에서 챕터 선택으로 게임을 시작하면 false
+    /// </summary>
+    bool isStoryMode;
+    public bool IsStoryMode => isStoryMode;
+
+    string path;
+    public UserDate userDate;
+
+    void RoadJson()
+    {
+        string json = File.ReadAllText(path);
+        userDate = JsonUtility.FromJson<UserDate>(json);
     }
 
-    // Update is called once per frame
-    void Update()
+    [ContextMenu("Save Json")]
+    void SaveJson()
     {
-        
+        string file = JsonUtility.ToJson(userDate);
+        File.WriteAllText(Application.persistentDataPath, file);
     }
+}
+
+[System.Serializable]
+public class UserDate
+{
+    public bool[] IsCleared = { false, false, false, false};
+    public float[] score = { 0f,0f,0f,0f};
 }
