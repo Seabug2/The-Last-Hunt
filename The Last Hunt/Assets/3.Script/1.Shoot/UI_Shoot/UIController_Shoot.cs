e38ddd5464d5ce1a6a2decfe55f597be3ee95709
JihoonSung <168630251+Erlo282@users.noreply.github.com>
2024-07-23 17:30:03 +0900

    [Update] Change UI Font / Add Cutscene

--- The Last Hunt/Assets/3.Script/1.Shoot/UI_Shoot/UIController_Shoot.cs
+++ The Last Hunt/Assets/3.Script/1.Shoot/UI_Shoot/UIController_Shoot.cs
@@ -18,6 +18,7 @@ public class UIController_Shoot : MonoBehaviour
 
     [SerializeField] private GameObject GameClear;
     [SerializeField] private Text GameClearScore_Text;
+    [SerializeField] private GameObject NextChapter;
     [SerializeField] private GameObject GameOver;
     [SerializeField] private Text GameOverScore_Text;
 
@@ -145,14 +146,25 @@ public class UIController_Shoot : MonoBehaviour
         // Save result to JSON
         if (TotalKill_Score >= 2000)
         {
+            if (!GameManager.instance.IsStoryMode)
+            {
+                NextChapter.SetActive(false);
+            }
             GameClear.SetActive(true);
             GameClearScore_Text.text = string.Format("Score : {0:#,##0}", TotalKill_Score);
+            //GameManager.instance.userDate.IsCleared[0] = true;
+            //GameManager.instance.SaveJson();
         }
         else
         {
             GameOver.SetActive(true);
             GameOverScore_Text.text = string.Format("Score : {0:#,##0}", TotalKill_Score);
         }
+    }
+
+    public void NextCh()
+    {
+        SceneManager.LoadScene("4.Next to #2");
     }
 
     public void Restart()
