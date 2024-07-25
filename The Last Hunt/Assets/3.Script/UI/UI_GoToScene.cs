602ea31fb80a64c916d1bcfa4d991f7d31392fb0
정해창 <femgoon0425@gmail.com>
2024-07-25 14:12:42 +0900

    씬 로드 전 페이드 아웃 추가 with DoTween

--- The Last Hunt/Assets/3.Script/UI/UI_GoToScene.cs
+++ The Last Hunt/Assets/3.Script/UI/UI_GoToScene.cs
@@ -1,17 +1,33 @@
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UI_GoToScene : MonoBehaviour
 {
     [SerializeField]
-    private int sceneNumber; // 인스펙터에서 설정할 씬 번호 변수
+    Image fadeBoard;
+
+    /// <summary>
+    /// 다음 씬으로 넘어가기 전 실행해야 하는 것들이 있으면 이 이벤트에 추가해주세요.
+    /// 추가 방법은 FadeOutEvent.AddListener() 라는 것을 쓰면 됩니다.
+    /// </summary>
+    public UnityEvent FadeOutEvent;
 
     // UI 버튼에 연결할 메서드
-    public void GoToScene()
+    public void GoToScene(int sceneNumber)
     {
+        FadeOutEvent?.Invoke();
         DOTween.KillAll();
         Time.timeScale = 1.0f;
-        SceneManager.LoadScene(sceneNumber); // 설정된 씬 번호로 이동
+        if (fadeBoard)
+        {
+            fadeBoard.DOFade(0, 5f).OnComplete(() => SceneManager.LoadScene(sceneNumber));
+        }
+        else
+        {
+            SceneManager.LoadScene(sceneNumber); // 설정된 씬 번호로 이동
+        }
     }
 }
